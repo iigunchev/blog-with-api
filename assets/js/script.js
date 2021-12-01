3ee26b6628ab5664df4d6990069001ba4cb36fe8
iigunchev <iigunchev@gmail.com>
2021-12-01 18:31:57 +0100

    loading comments and post image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,36 @@
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
 
 fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => response.json())
-  .then(json => {
-    $( "#postTitle" ).text(json.title);
-    $( ".postBody" ).text(json.body);
-  })
+    .then(response => response.json())
+    .then(json => {
+        $( "#postTitle" ).text(json.title);
+        $( ".postBody" ).text(json.body);
+})
 
-  fetch('https://jsonplaceholder.typicode.com/users/1')
-  .then(response => response.json())
-  .then(json => {
+fetch('https://jsonplaceholder.typicode.com/users/1')
+    .then(response => response.json())
+    .then(json => {
     $( "#username" ).text(json.username);
     $( "#email" ).text(json.email);
-  })
+})
+
+    $( "#loadComments" ).on('click', () => {
+        fetch('https://jsonplaceholder.typicode.com/comments?postId=1')
+        .then(response => response.json())
+        .then(json => {
+            json.forEach(comment => {
+                const commentDiv = $( `
+                <div class='comment'>
+                <h5>${comment.name}</h5>
+                <p>${comment.body}</p>
+                <span><i>${comment.email}</i></span>
+                <hr>
+                </div>
+                ` );
+                $( ".modal-footer" ).prepend(commentDiv);
+            })
+        })
+})
