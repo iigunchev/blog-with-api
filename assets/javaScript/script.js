cdc9e93a17d5cf2f7835bf0b3d46f4054d8dd344
Bertinoti <jeffersonbertinoti@gmail.com>
2021-12-02 17:34:52 +0100

    randon img by page

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -1,19 +1,37 @@
+var imgdirection=[]
+function imgRandon() {
+    page= Math.floor((Math.random() * 482) + 1);
+    console.log(page)
+    fetch("https://api.unsplash.com/search/photos?query=travelling&page="+page+"&client_id=KqhYMraQy6eKA2IZ9lFNthSDFdwf9SksbYpVB7qmr4M")
+    .then(response => response.json())
+    .then(result =>{
+        //let imgNum= Math.floor((Math.random() * 9) + 1);
+        // imgdirection.push(result.results[imgNum].urls.regular)
+        cont= 0
+        while (cont < 9){
 
-function randonImg() {
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
-
-    fetch("https://picsum.photos/200/300", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+            imgdirection.push(result.results[cont].urls.regular)
+            cont ++
+        }
+        console.log(imgdirection)
+    })
+    .catch(error => console.log('error', error));
 }
-var randonimg= ''
 
-function insertimg(id) {
-    img = document.getElementById('id')
-    randonimg= randonImg()
-    img.setAttribute('src', randonimg )
+async function runImg() {
+    imgRandon()
+    createImg()
 }
+
+
+function createImg() {
+    var ImgTag = document.getElementsByClassName("imgtoCreate");
+        setTimeout(() => {
+            for (i = 0; i < ImgTag.length; i++) {
+                ImgTag[i].setAttribute('src', imgdirection[i]);
+            }
+        }, 500);
+
+}
+
+runImg()
