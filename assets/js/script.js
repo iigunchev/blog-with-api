d84e508271f6cfb0324d5d990a82d6aeb0656adf
iigunchev <iigunchev@gmail.com>
2021-12-03 09:42:01 +0100

    update and delet post added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,55 @@
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-tt="tooltip"]')
+);
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+// Fetching post elements from API
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+  .then((response) => response.json())
+  .then((json) => {
+    $("#postTitle").text(json.title);
+    $(".postBody").text(json.body);
+  });
 
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-    .then(response => response.json())
-    .then(json => {
-        $( "#postTitle" ).text(json.title);
-        $( ".postBody" ).text(json.body);
-})
-
-fetch('https://jsonplaceholder.typicode.com/users/1')
-    .then(response => response.json())
-    .then(json => {
-    $( "#username" ).text(json.username);
-    $( "#email" ).text(json.email);
-})
-
-    $( "#loadComments" ).on('click', () => {
-        fetch('https://jsonplaceholder.typicode.com/comments?postId=1')
-        .then(response => response.json())
-        .then(json => {
-            json.forEach(comment => {
-                const commentDiv = $( `
+fetch("https://jsonplaceholder.typicode.com/users/1")
+  .then((response) => response.json())
+  .then((json) => {
+    $("#username").html(`<u>Post by</u> <b>${json.username}</b>  |`);
+    $("#email").html(`  <i>${json.email}</i>`);
+  });
+// Load / hide comments
+$("#loadComments").on("click", () => {
+  fetch("https://jsonplaceholder.typicode.com/comments?postId=1")
+    .then((response) => response.json())
+    .then((json) => {
+      json.forEach((comment) => {
+        commentDiv = $(`
                 <div class='comment'>
                 <h5>${comment.name}</h5>
                 <p>${comment.body}</p>
                 <span><i>${comment.email}</i></span>
                 <hr>
                 </div>
-                ` );
-                $( ".modal-footer" ).prepend(commentDiv);
-            })
-        })
-})
+                `);
+        $(".modal-footer").append(commentDiv);
+      });
+    });
+});
+// Update post
+$("#updatePostBtn").one("click", () => {
+  $("#alert-update-success").removeClass("hide");
+  $("#alert-update-success").addClass("show");
+  setTimeout(() => {
+    $("#alert-update-success").fadeOut(2000);
+  }, 1000);
+});
+// Delete post
+$("#deletePostBtn").one("click", () => {
+  $("#alert-update-success").text("The post has been successfully deleted");
+  $("#alert-update-success").removeClass("hide");
+  $("#alert-update-success").addClass("show");
+  setTimeout(() => {
+    $("#alert-update-success").fadeOut(2000);
+  }, 1000);
+});
