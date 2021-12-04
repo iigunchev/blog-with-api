16bcc36780c55bbb284cb35b1430c8ffe1a27c93
iigunchev <iigunchev@gmail.com>
2021-12-04 10:12:41 +0100

    list structure and fetching photos and content

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,3 +55,43 @@ $("#deletePostBtn").one("click", () => {
     $("#alert-update-success").fadeOut(2000);
   }, 1000);
 });
+
+// Blog list generation
+function generateBlogPosts() {
+  for (let i = 0; i < 10; i++) {
+    let imgSrc = ''
+    fetch('https://api.unsplash.com/search/photos?query=travelling&orientation=squarish&client_id=riw-OTey4wubCdHBzxIgq1Es8wEk_poyi-HJjww1c98')
+    .then(response => response.json())
+    .then(data => imgSrc = data.results[i].urls.small)
+    fetch('https://jsonplaceholder.typicode.com/posts/')
+        .then(response => response.json())
+        .then(data => {
+            let blogPost = $(`      
+                <a href="#" class="list-group-item list-group-item-action" aria-current="true" data-bs-toggle="modal" data-bs-target="#postModal">
+                <div class="d-flex w-100 justify-content-between">
+                    <div class="d-flex col-9">
+                        <img src="${imgSrc}" class="" alt="..." width="100" height="100">
+                        <div class='ms-3'>
+                        <h5 class="mb-1">${data[i].title}</h5>
+                        <p class="mb-1">${data[i].body}</p>
+                        </div>
+                    </div>
+                    <small class="col-2 text-end">
+                        <button type="button" class="btn" data-bs-toggle="modal" data-tt="tooltip"
+                            data-bs-target="#editModal" data-bs-placement="top" title="Edit post">
+                            <span class="material-icons"> edit </span>
+                        </button>
+                        <button type="button" id="deletePostBtn" class="btn" data-bs-dismiss="modal" data-tt="tooltip"
+                            data-bs-placement="top" title="Delete post">
+                            <span class="material-icons"> delete_forever </span>
+                        </button>
+                    </small>
+                </div>
+                </a>
+                `);
+            $(".list-group").append(blogPost);
+        })
+    }
+}
+
+generateBlogPosts();
