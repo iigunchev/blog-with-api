18970106e8c0dac4b9d7c8e591e66969e7ba2952
iigunchev <iigunchev@gmail.com>
2021-12-09 09:48:34 +0100

    remove buttons from edit modal, add tooltips

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,3 @@
-var tooltipTriggerList = [].slice.call(
-  document.querySelectorAll('[data-tt="tooltip"]')
-);
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl);
-});
-
-// Fetching post elements from API
-
-// function createPostInfo(post) {
-//   fetch(`https://jsonplaceholder.typicode.com/posts/${post}`)
-//     .then((response) => response.json())
-//     .then((json) => {
-//       $("#postTitle").text(json.title);
-//       $(".postBody").text(json.body);
-//     });
-// }
-// fetch("https://jsonplaceholder.typicode.com/users/1")
-//   .then((response) => response.json())
-//   .then((json) => {
-//     $("#username").html(`<u>Post by</u> <b>${json.username}</b>  |`);
-//     $("#email").html(`  <i>${json.email}</i>`);
-//   });
-// // Load / hide comments
-// $("#loadComments").on("click", () => {
-//   fetch("https://jsonplaceholder.typicode.com/comments?postId=1")
-//     .then((response) => response.json())
-//     .then((json) => {
-//       json.forEach((comment) => {
-//         commentDiv = $(`
-//                 <div class='comment'>
-//                 <h5>${comment.name}</h5>
-//                 <p>${comment.body}</p>
-//                 <span><i>${comment.email}</i></span>
-//                 <hr>
-//                 </div>
-//                 `);
-//         $(".modal-footer").append(commentDiv);
-//       });
-//     });
-// });
-// Update post alert
-// $("#updatePostBtn").one("click", () => {
-//   $("#alert-update-success").removeClass("hide");
-//   $("#alert-update-success").addClass("show");
-//   setTimeout(() => {
-//     $("#alert-update-success").fadeOut(2000);
-//   }, 1000);
-// });
-// // Delete post
-// $("#deletePostBtn").one("click", () => {
-//   $("#alert-update-success").text("The post has been successfully deleted");
-//   $("#alert-update-success").removeClass("hide");
-//   $("#alert-update-success").addClass("show");
-//   setTimeout(() => {
-//     $("#alert-update-success").fadeOut(2000);
-//   }, 1000);
-// });
 let page = 1;
 let img = 0;
 generateBlogPosts(page);
@@ -125,8 +67,8 @@ function initAll() {
         $("#postImg").attr("src", $(`#imgPost${postId}`).attr("src"));
         $("#postTitle").text($(`#titlePost${postId}`).text());
         $(".postBody").text($(`#bodyPost${postId}`).text());
-        $("#username").text($(`.author${postId}`).text())
-        $("#username").addClass("italic")
+        $("#username").text($(`.author${postId}`).text());
+        $("#username").addClass("italic");
         $("#loadComments").one("click", () => {
           fetch(
             `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -156,6 +98,14 @@ function initAll() {
 
     //   Delete button
     initDeleteButtons(deleteBtns, postId);
+
+    //   Tooltips
+    let tooltipTriggerList = [].slice.call(
+      document.querySelectorAll('[data-tt="tooltip"]')
+    );
+    let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
   }, 1000);
 }
 
